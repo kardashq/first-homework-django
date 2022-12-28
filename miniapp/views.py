8bde783d3549ea483a5e672be1a72b016aae75c7
kardashq <kardashq@yandex.ru>
2022-12-28 21:34:35 +0300

    change: func

--- miniapp/views.py
+++ miniapp/views.py
@@ -3,22 +3,21 @@ from miniapp.models import Event
 
 
 def childhood(request):
-    objs = Event.objects.filter(category=1).all
+    objs = Event.objects.filter(category__title='childhood')
     if objs:
         return render(request, 'childhood.html', {'obj': objs})
-        #return HttpResponse(objs)
     return HttpResponse('<h1>Not found</h1>', status=404)
 
 
 def youth(request):
-    objs = Event.objects.filter(category=2)
+    objs = Event.objects.filter(category__title='youth')
     if objs:
         return render(request, 'youth.html', {'obj': objs})
     return HttpResponse('<h1>Not found</h1>', status=404)
 
 
 def adulthood(request):
-    objs = Event.objects.filter(category=3)
+    objs = Event.objects.filter(category__title='adulthood')
     if objs:
         return render(request, 'adulthood.html', {'obj': objs})
     return HttpResponse('<h1>Not found</h1>', status=404)
